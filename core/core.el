738f28afe33ce453b7097db1bad22f1a8a265d33
Erick Bodine <erick.bodine@gmail.com>
2019-10-26 19:30:52 -0600

    clojure support

--- core/core.el
+++ core/core.el
@@ -106,9 +106,8 @@ ability to invoke the debugger in debug mode."
   (setq inhibit-startup-message t
         inhibit-startup-echo-area-message user-login-name
         inhibit-default-init t
-        initial-major-mode 'fundamental-mode
         initial-scratch-message nil
-	mode-line-format nil))
+        mode-line-format nil))
 
 (setq-default
  ad-redefinition-action 'accept   ; silence advised function warnings
