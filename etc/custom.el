fd6f4c865f6ce83d042191eba549352bc608c71a
Erick Bodine <erick.bodine@gmail.com>
2023-09-30 10:53:37 -0600

    safe themes change

--- etc/custom.el
+++ etc/custom.el
@@ -3,8 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("a44e2d1636a0114c5e407a748841f6723ed442dc3a0ed086542dc71b92a87aee" default)))
+ '(bmkp-last-as-first-bookmark-file "~/MyStuff/emacsrc/var/bookmark-default.el"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
