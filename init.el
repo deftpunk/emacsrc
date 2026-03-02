0f7df5a9cf46d650649e51a52165f03a3080346a
Erick Bodine <erick.bodine@gmail.com>
2026-03-02 09:14:14 -0700

    adding imenu-list header but not code

--- init.el
+++ init.el
@@ -2459,16 +2459,3 @@ The DWIM behaviour of this command is as follows:
 (run-with-idle-timer 1.2 t 'garbage-collect)
 
 (server-start)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("5c7720c63b729140ed88cf35413f36c728ab7c70f8cd8422d9ee1cedeb618de5" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
